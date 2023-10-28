c6b3b61ef7cab7c32174765cead0e0d1a1fa4eaf
Suhas <saisuhas.1324@gmail.com>
2023-10-28 19:22:07 +0530

    now the user can add images and videos for their blogs

--- cms_project/cms_app/models.py
+++ cms_project/cms_app/models.py
@@ -3,15 +3,16 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# Create your models here.
 class Post(models.Model):
-    title=models.CharField(max_length=100)
-    content=models.TextField()
-    date_posted=models.DateTimeField(default=timezone.now)
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    video = models.FileField(upload_to='blog_videos/', blank=True, null=True)
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
--- cms_project/cms_app/views.py
+++ cms_project/cms_app/views.py
@@ -5,37 +5,42 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-
 def home(request):
-    context={
-        'posts': Post.objects.all()
+    context = {
+        'posts': Post.objects.all().order_by('-date_posted')
     }
     return render(request, 'cms_app/home.html', context)
 
 class PostListView(ListView):
     model = Post
-    template_name='cms_app/home.html'
-    context_object_name='posts'
-    ordering=['-date_posted']
-    paginate_by=5
+    template_name = 'cms_app/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
 
 class UserPostListView(ListView):
     model = Post
-    template_name='cms_app/user_posts.html'
-    context_object_name='posts'
-    ordering=['-date_posted']
-    paginate_by=5
+    template_name = 'cms_app/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
 
     def get_queryset(self):
-        user=get_object_or_404(User, username=self.kwargs.get('username'))
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Post
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields=['title', 'content']
+    template_name = 'cms_app/post_form.html'
+    fields = ['title', 'content', 'image', 'video']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -43,17 +48,17 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields=['title', 'content']
+    template_name = 'cms_app/post_form.html'
+    fields = ['title', 'content', 'image', 'video']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
     def test_func(self):
-        post=self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
+        # Implement your permission check logic here
+        # For example, you can check if the user is the author of the post
+        return self.request.user == self.get_object().author
+
     
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
